62d819a55d021672c97fe767a79349820ec1f955
faizvk <faizvk14@gmail.com>
2025-08-16 02:36:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ function insert_Row() {
     let cell1 = newRow.insertCell(0);
     let cell2 = newRow.insertCell(1);
 
-    cell1.textContent = "New cell1";
-    cell2.textContent = "New cell2"; 
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2"; 
 }
